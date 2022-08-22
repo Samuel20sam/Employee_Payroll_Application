49d3c6ef3c2d24dafcc81d54e79482bdb3b16993
Samuel_Elijah <samuel20sam@gmail.com>
2022-08-22 13:01:03 +0530

    Adding: Add Validation to Name Field so the REST call can be validated

--- PayRoll/src/main/java/com/bridgelabz/payroll/EmployeePayrollApplication.java
+++ PayRoll/src/main/java/com/bridgelabz/payroll/EmployeePayrollApplication.java
@@ -11,7 +11,8 @@ public class EmployeePayrollApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class, args);
-        log.info("Employee Payroll App Started in {} Environment", context.getEnvironment().getProperty("environment"));
         System.out.println("\nWelcome to Employee Payroll application");
+        log.info("Employee Payroll App Started in {} Environment", context.getEnvironment().getProperty("environment"));
+        log.info("Employee Payroll DB User is {}", context.getEnvironment().getProperty("spring.datasource.username"));
     }
 }
